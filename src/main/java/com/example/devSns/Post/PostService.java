caab85a5eee1ece680c00b31e9ea7624ee630c4a
tomchccom <dreamkms2014@gmail.com>
2025-11-11 22:32:32 +0900

    fix: 게시글 컨트롤러 수정 , 트랙잭션 적용 - 어떤 멤버가 작성한 글인지 저장하도록 로직 수정 -

--- src/main/java/com/example/devSns/Post/PostService.java
+++ src/main/java/com/example/devSns/Post/PostService.java
@@ -1,36 +1,40 @@
 package com.example.devSns.Post;
 
 import com.example.devSns.Comment.CommentRepository;
+import com.example.devSns.Heart.HeartRepository;
+import com.example.devSns.Heart.LikeStatus;
+import com.example.devSns.Member.Member;
+import com.example.devSns.Member.MemberRepository;
 import com.example.devSns.Post.Dto.AddPostRequestDto;
 import com.example.devSns.Post.Dto.GetPostResponseDto;
 import com.example.devSns.Post.Dto.UpdatePostRequestDto;
 import com.example.devSns.global.EntityNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class PostService {
 
     private final PostRepository postRepository;
     private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
+    private final HeartRepository heartRepository;
 
-
-    @Autowired
-    public PostService(PostRepository postRepository,CommentRepository commentRepository) {
-        this.postRepository = postRepository;
-        this.commentRepository = commentRepository;
-    }
-    public void createPost(AddPostRequestDto Dto) {
+    @Transactional
+    public void createPost(AddPostRequestDto Dto, Long memberId) {
         Post post = new Post(
                 Dto.content(),
                 Dto.username()
         );
         postRepository.save(post);
     }
-
+    @Transactional(readOnly = true)
     public GetPostResponseDto findById(Long id) {
 
         Post post = postRepository.findById(id)
@@ -39,13 +43,13 @@ public class PostService {
 
         return new GetPostResponseDto(
                 post.getContent(),
-                post.getLikeCount(),
+                post.getLikeCount(), // 이거 나중에 외래키로 계산해서 보내기, Dto 변환로직 만들어 두자
                 post.getUserName(),
                 post.getCreatedAt(),
                 commentRepository.findByPostIdAndParentIsNull(post.getId())
         );
     }
-
+    @Transactional(readOnly = true)
     public List<GetPostResponseDto> findAll() {
         return postRepository.findAll()
                 .stream()
@@ -67,6 +71,7 @@ public class PostService {
         postRepository.delete(post);
     }
 
+    @Transactional
     public void updatePost(Long id , UpdatePostRequestDto Dto) {
         Post post = postRepository.findById(id)
                 .orElseThrow(() ->new EntityNotFoundException("게시글이 존재하지 않습니다"));
